146b9c6c95388405a9c5efbb6fc59b457f9c2cd8
Koenraad Haedens <koenraad@khd.be>
2024-07-24 12:17:22 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SplitScreenApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
